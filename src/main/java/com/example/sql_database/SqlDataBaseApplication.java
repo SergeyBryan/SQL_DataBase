c2a7a2627b4649b922f9b4d6b5e32780f2e7577c
sergeybryan <regkmastis@yandex.ru>
2023-06-28 16:33:12 +0300

    Initial commit

---
+++ src/main/java/com/example/sql_database/SqlDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sql_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlDataBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlDataBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sql_database/SqlDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlDataBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
