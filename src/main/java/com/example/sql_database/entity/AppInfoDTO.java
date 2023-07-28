6354a4a10e50308afa45c6fe08d705ecc63ad7cd
sergeybryan <regkmastis@yandex.ru>
2023-07-28 12:52:46 +0300

    Домашняя работа logger

---
+++ src/main/java/com/example/sql_database/entity/AppInfoDTO.java
@@ -0,0 +1,26 @@
+package com.example.sql_database.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AppInfoDTO {
+
+    private String appName;
+    private String appVersion;
+    private String appEnvironment;
+
+    public static AppInfoDTO info(String appEnvironment) {
+        AppInfoDTO appInfoDTO = new AppInfoDTO();
+        appInfoDTO.setAppEnvironment(appEnvironment);
+        if (appEnvironment.equals("dev")) {
+            appInfoDTO.setAppName("hogwarts-school");
+            appInfoDTO.setAppVersion("0.0.1");
+            return appInfoDTO;
+        }
+        return appInfoDTO;
+    }
+}
--- src/main/java/com/example/sql_database/service/StudentService.java
+++ src/main/java/com/example/sql_database/service/StudentService.java
@@ -5,6 +5,8 @@ import com.example.sql_database.entity.Student;
 import com.example.sql_database.entity.StudentDTO;
 import com.example.sql_database.repository.FacultyRepository;
 import com.example.sql_database.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springdoc.api.OpenApiResourceNotFoundException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,7 @@ import java.util.List;
 
 @Service
 public class StudentService {
-
+    private final Logger logger = LoggerFactory.getLogger(StudentService.class);
     private final StudentRepository studentRepository;
     private final FacultyRepository facultyRepository;
 
@@ -26,9 +28,11 @@ public class StudentService {
     public StudentDTO getStudent(Long id) {
         Student student = studentRepository.findById(id).orElse(null);
         if (student != null) {
+            logger.info("Was invoked method for get student");
             return StudentDTO.fromStudent(student);
         } else {
-            throw new OpenApiResourceNotFoundException("Студент с id " + id + " не найдён");
+            logger.error("There is not student with id = " + id);
+            throw new OpenApiResourceNotFoundException("This id doesn't exist");
         }
     }
 
@@ -36,10 +40,12 @@ public class StudentService {
         List<Student> students = studentRepository.findAll();
         List<StudentDTO> studentDTOS = new ArrayList<>();
         students.forEach(student -> studentDTOS.add(StudentDTO.fromStudent(student)));
+        logger.info("Was invoked method for get all students");
         return studentDTOS;
     }
 
     public List<StudentDTO> getByAge(int age) {
+        logger.info("Was invoked method for get student by age");
         List<Student> students = studentRepository.findStudentByAge(age);
         List<StudentDTO> studentDTOS = new ArrayList<>();
         students.forEach(student -> studentDTOS.add(StudentDTO.fromStudent(student)));
@@ -47,6 +53,7 @@ public class StudentService {
     }
 
     public List<StudentDTO> getByAgeBetween(int min, int max) {
+        logger.info("Was invoked method for get student by age between min and max value");
         List<Student> students = studentRepository.findStudentsByAgeBetween(min, max);
         List<StudentDTO> studentDTOS = new ArrayList<>();
         students.forEach(student -> studentDTOS.add(StudentDTO.fromStudent(student)));
@@ -56,13 +63,15 @@ public class StudentService {
     public FacultyDTO getStudentFaculty(Long id) {
         Student student = studentRepository.findById(id).orElse(null);
         if (student != null) {
+            logger.info("Was invoked method for get student faculty");
             FacultyDTO facultyDTO = new FacultyDTO();
             facultyDTO.setId(student.getFaculty().getId());
             facultyDTO.setName(student.getFaculty().getName());
             facultyDTO.setColor(student.getFaculty().getColor());
             return facultyDTO;
         } else {
-            throw new OpenApiResourceNotFoundException("Студент с id " + id + " не найден");
+            logger.error("There is not student with id = " + id);
+            throw new OpenApiResourceNotFoundException("This id doesn't exist");
         }
     }
 
@@ -71,13 +80,16 @@ public class StudentService {
             Student student = studentDTO.toStudent();
             student.setId(id);
             student.setFaculty(facultyRepository.findById(studentDTO.getFacultyId()).orElse(null));
+            logger.info("Was invoked method for update student");
             studentRepository.save(student);
         } else {
-            throw new OpenApiResourceNotFoundException("Студент с id " + id + " не найдён");
+            logger.error("There is not student with id = " + id);
+            throw new OpenApiResourceNotFoundException("This id doesn't exist");
         }
     }
 
     public void delete(Long id) {
+        logger.info("Was invoked method for delete student");
         studentRepository.deleteById(id);
     }
 
@@ -86,19 +98,23 @@ public class StudentService {
         student.setAge(studentDTO.getAge());
         student.setName(studentDTO.getName());
         student.setFaculty((facultyRepository.findById(studentDTO.getFacultyId()).orElse(null)));
+        logger.info("Was invoked method for create student");
         studentRepository.save(student);
     }
 
 
     public Integer getNumberOfStudents() {
+        logger.info("Was invoked method for count number of students");
         return studentRepository.getNumberOfStudents();
     }
 
     public Integer getAverageAgeByStudent() {
+        logger.info("Was invoked method for count average age by student");
         return studentRepository.getAverageAgeByStudent();
     }
 
     public List<StudentDTO> getYoungStudents() {
+        logger.info("Was invoked method for get five young students");
         List<Student> students = studentRepository.getFiveYoungStudents();
         List<StudentDTO> studentDTOS = new ArrayList<>();
         students.forEach(student -> studentDTOS.add(StudentDTO.fromStudent(student)));
@@ -106,7 +122,9 @@ public class StudentService {
     }
 
     public List<StudentDTO> getAllBySort(Integer pageNumber, Integer studentQuantity) {
+        logger.info("Was invoked method for get students by page and quantity per each page");
         if (studentQuantity > 50 || studentQuantity <= 0) {
+            logger.warn("It's not possible to display more than 50 students or a negative value. The quantity per page has been adjusted to show 50 students.");
             studentQuantity = 50;
         }
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, studentQuantity);
